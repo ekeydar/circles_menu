856adbe472385c7f371f8cedd1d3a7a62020b410
Eran Keydar <ekeydar@gmail.com>
2021-09-12 09:06:49 +0300

    v1

---
+++ lib/circles_menu.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CirclesMenu extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _CirclesMenuState();
+}
+
+class _CirclesMenuState extends State<CirclesMenu> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('circles menu')
+    );
+  }
+}
