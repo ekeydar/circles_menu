b2659c4834d8882bab3238f505e0eb80764e6fff
Eran Keydar <ekeydar@gmail.com>
2021-09-14 15:21:02 +0300

    search

--- lib/menu_page_scaffold.dart
+++ lib/menu_page_scaffold.dart
@@ -31,75 +31,77 @@ class _MenuPageScaffoldState extends State<MenuPageScaffold> {
       ),
       body: _ready
           ? CirclesMenu(
-              dataList: dataList,
-              onPressed: (OpState op) {
-                op.action.onPress();
-              },
-              onChange: () {
-                dataList.removeWhere((d) => d.isDeleted);
-                _dumpopStateList();
-                setState(() {});
-              })
+          dataList: dataList,
+          onPressed: (OpState op) {
+            op.action.onPress();
+          },
+          onChange: () {
+            dataList.removeWhere((d) => d.isDeleted);
+            _dumpOpStateList();
+            setState(() {});
+          })
           : Center(
-              child: Text('loading...'),
-            ),
+        child: Text('loading...'),
+      ),
       floatingActionButton: _ready
           ? Padding(
-              padding: const EdgeInsets.only(left: 16.0, right: 16.0),
-              child: Row(
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.only(left: 8.0, right: 8),
-                    child: FloatingActionButton(
-                      onPressed: () {
-                        dataList.clear();
-                        _dumpopStateList();
-                        setState(() {});
-                      },
-                      backgroundColor: Colors.red,
-                      child: Icon(Icons.delete),
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.only(left: 8.0, right: 8),
-                    child: FloatingActionButton(
-                      onPressed: () async {
-                        OpAction? newAction = await pickAction();
-                        //debugPrint('action = $newAction');
-                        if (newAction != null) {
-                          int index = dataList.length;
-                          dataList.add(OpState(
-                            action: newAction,
-                            x: 100 + index * 10,
-                            y: 100,
-                            radius: 100,
-                            fillColor: Theme.of(context).primaryColor,
-                          ));
-                          _dumpopStateList();
-                          setState(() {});
-                        }
-                      },
-                      backgroundColor: Colors.green,
-                      child: Icon(Icons.add),
-                    ),
-                  ),
-                  if (kDebugMode)
-                    Padding(
-                      padding: const EdgeInsets.only(left: 8.0, right: 8),
-                      child: FloatingActionButton(
-                        onPressed: () {
-                          // debugPrint('size = ${MediaQuery.of(context).size}');
-                          for (var d in dataList) {
-                            debugPrint('${d.text}: ${d.x} ${d.y}');
-                          }
-                        },
-                        backgroundColor: Colors.green,
-                        child: Icon(Icons.bug_report_outlined),
-                      ),
-                    )
-                ],
+        padding: const EdgeInsets.only(left: 16.0, right: 16.0),
+        child: Row(
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(left: 8.0, right: 8),
+              child: FloatingActionButton(
+                onPressed: () {
+                  dataList.clear();
+                  _dumpOpStateList();
+                  setState(() {});
+                },
+                backgroundColor: Colors.red,
+                child: Icon(Icons.delete),
               ),
-            )
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 8.0, right: 8),
+              child: FloatingActionButton(
+                onPressed: () async {
+                  OpAction? newAction = await pickAction();
+                  //debugPrint('action = $newAction');
+                  if (newAction != null) {
+                    int index = dataList.length;
+                    dataList.add(OpState(
+                      action: newAction,
+                      x: 100 + index * 10,
+                      y: 100,
+                      radius: 100,
+                      fillColor: Theme
+                          .of(context)
+                          .primaryColor,
+                    ));
+                    _dumpOpStateList();
+                    setState(() {});
+                  }
+                },
+                backgroundColor: Colors.green,
+                child: Icon(Icons.add),
+              ),
+            ),
+            if (kDebugMode)
+              Padding(
+                padding: const EdgeInsets.only(left: 8.0, right: 8),
+                child: FloatingActionButton(
+                  onPressed: () {
+                    // debugPrint('size = ${MediaQuery.of(context).size}');
+                    for (var d in dataList) {
+                      debugPrint('${d.text}: ${d.x} ${d.y}');
+                    }
+                  },
+                  backgroundColor: Colors.green,
+                  child: Icon(Icons.bug_report_outlined),
+                ),
+              )
+          ],
+        ),
+      )
           : null,
     );
   }
@@ -114,32 +116,11 @@ class _MenuPageScaffoldState extends State<MenuPageScaffold> {
         context: context,
         barrierDismissible: false,
         builder: (context) {
-          return AlertDialog(
-            actions: [
-              TextButton(
-                child: Text('cancel'),
-                onPressed: () {
-                  Navigator.of(context).pop(null);
-                },
-              ),
-            ],
-            title: Text('pick category'),
-            content: SingleChildScrollView(
-              child: Column(
-                  children: actions
-                      .map((a) => ListTile(
-                            title: Text(a.title),
-                            onTap: () {
-                              Navigator.of(context).pop(a);
-                            },
-                          ))
-                      .toList()),
-            ),
-          );
+          return AlertDialogWithSearch(actions: actions);
         });
   }
 
-  Future<void> _dumpopStateList() async {
+  Future<void> _dumpOpStateList() async {
     SharedPreferences sp = await SharedPreferences.getInstance();
     String value = jsonEncode(dataList.map((m) => m.toMap()).toList());
     await sp.setString(spKey, value);
@@ -147,7 +128,7 @@ class _MenuPageScaffoldState extends State<MenuPageScaffold> {
 
   Future<void> _prepare() async {
     await _buildActions();
-    await _buildopStateList();
+    await _buildOpStateList();
     setState(() {
       _ready = true;
     });
@@ -165,7 +146,7 @@ class _MenuPageScaffoldState extends State<MenuPageScaffold> {
     }
   }
 
-  Future<void> _buildopStateList() async {
+  Future<void> _buildOpStateList() async {
     SharedPreferences sp = await SharedPreferences.getInstance();
     String? text = sp.getString(spKey);
     if (text == null) {
@@ -177,16 +158,92 @@ class _MenuPageScaffoldState extends State<MenuPageScaffold> {
       dataList = dataMaps
           .where((m) => actionsByCode.containsKey(m['actionCode']))
           .map(
-            (m) => OpState(
+            (m) =>
+            OpState(
               x: m['x'],
               y: m['y'],
               radius: m['radius'],
               action: actionsByCode[m['actionCode']]!,
               fillColor: Color(
-                  m['fillColorValue'] ?? Theme.of(context).primaryColor.value),
+                  m['fillColorValue'] ?? Theme
+                      .of(context)
+                      .primaryColor
+                      .value),
             ),
-          )
+      )
           .toList();
     }
+  }
+}
+
+class AlertDialogWithSearch extends StatefulWidget {
+  final List<OpAction> actions;
+  AlertDialogWithSearch({required this.actions});
+
+  @override
+  State<StatefulWidget> createState() => _AlertDialogWithSearchState();
+}
+
+
+class _AlertDialogWithSearchState extends State<AlertDialogWithSearch> {
+  late TextEditingController _controller;
+  @override
+  void initState() {
+    _controller = TextEditingController();
+    super.initState();
+    _controller.addListener(() {
+      setState(() {});
+    });
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    debugPrint('In build q = ${_controller.text}');
+    return AlertDialog(
+      actions: [
+        TextButton(
+          child: Text('cancel'),
+          onPressed: () {
+            Navigator.of(context).pop(null);
+          },
+        ),
+      ],
+      title: Text('pick category'),
+      content: Container(
+        height: MediaQuery.of(context).size.height * 0.7,
+        child: SingleChildScrollView(
+          child: Column(
+              children: <Widget>[
+                TextField(
+                  controller: _controller,
+                  decoration: InputDecoration(
+                    suffixIcon: IconButton(
+                        onPressed: () {
+                            _controller.text = '';
+                            setState(() {});
+                        },
+                        icon: Icon(Icons.clear)
+                    ),
+                  ),
+                )
+              ] + widget.actions
+                  .where((a) => _controller.text.length == 0 || a.title.contains(_controller.text))
+                  .map((a) =>
+                  ListTile(
+                    title: Text(a.title),
+                    onTap: () {
+                      Navigator.of(context).pop(a);
+                    },
+                  ))
+                  .toList()),
+        ),
+      ),
+    );
   }
 }
